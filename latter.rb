5a279b9e95f742243f2bd5f30466af381d530f8f
Josh McArthur <joshua.mcarthur@gmail.com>
2011-03-28 17:46:42 +1300

    Fixing Pony's HTML escaping of body contents. Refactoring application and a couple of bugfixes.

--- latter.rb
+++ latter.rb
@@ -15,8 +15,6 @@ APP_DIR = File.expand_path(File.dirname(__FILE__))
 PUBLIC_DIR = File.join(APP_DIR, 'public')
 MODELS_DIR = File.join(APP_DIR, 'models')
 
-MAIL_FROM = "updates@latter.joshmcarthur.me"
-
 require File.join(MODELS_DIR, 'player.rb')
 require File.join(MODELS_DIR, 'challenge.rb')
 
@@ -27,43 +25,60 @@ enable :static
 
 ##### Latter: A Table Tennis Ladder ############
 
+
+######### Configuration #########
+
+
+
 I18N = {
   :record_not_found => "Record not found",
   :record_not_saved => "Record could not be saved. Please check data and try again."
 }
 
-######### Configuration #########
+
 configure :test do
   DataMapper.setup(:default, "sqlite3::memory")
   DataMapper.auto_upgrade!
 end
 
+
+configure :production do
+  PONY_SMTP_OPTIONS = {
+    :address => "smtp.sendgrid.net",
+    :port => '25',
+    :authentication => :plain,
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => ENV['SENDGRID_DOMAIN']
+  }
+end
+
 configure do
-  DataMapper.setup(:default, ENV["DATABASE_URL"] || "sqlite3://#{Dir.pwd}/db/latter.db.sqlite3")
+  DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/db/latter.db.sqlite3")
   DataMapper.auto_upgrade!
+  PONY_SMTP_OPTIONS = {}
 end
 
 before do
   current_player!
-  authenticate! unless request.path =~ /\A\/\Z/ || request.path =~ /\A\/login\Z/ || request.path =~ /\A\/setup\Z/
+  authenticate! if authenticate?(request)
 end
 
 
 get '/' do
-  redirect '/players' and return if @current_player
-  haml :"auth/login"
+  @current_player ? redirect('/players') : haml(:"auth/login")
 end
 
 get '/setup' do
-  if Player.all.length == 0
-    Player.create(:name => 'Admin', :email => 'admin@example.org')
-  end
+  Player.create(
+    :name => 'Admin',
+    :email => 'admin@example.org'
+  ) if Player.count.zero?
   haml :"auth/login"
 end
 
 post '/login' do
-  @current_player = Player.first(:email => params[:email].downcase)
-  if @current_player
+  if @current_player = Player.first(:email => params[:email].downcase)
     session[:player_id] = @current_player.id
     redirect '/players'
   else
@@ -107,7 +122,6 @@ get '/player/:id' do
 end
 
 post '/player' do
-  puts params
   @player = Player.create(params[:player])
   @player.saved? ? redirect("/player/#{@player.id}") : error(400, I18N[:record_not_saved])
 end
@@ -158,7 +172,9 @@ post '/challenge' do
   @challenge.to_player = Player.get(params[:challenge][:to_player_id])
   @challenge.completed = false
   if @challenge.save
-    send_mail(:to => @challenge.to_player.email,
+    send_mail(
+      :to => @challenge.to_player.email,
+      :from => @challenge.from_player.email,
       :subject => "New Challenge on Latter",
       :template => 'new_challenge',
       :object => @challenge
@@ -179,8 +195,18 @@ post '/challenge/:id/update' do
     :to_player_score => params[:challenge][:to_player_score]
   )
   @challenge.completed = true
-  challenge_updated = @challenge.save
-  challenge_updated ? send_mail(:to => @challenge.to_player.email, :subject => "Updated Challenge on Latter", :template => 'challenge_updated', :object => @challenge) && redirect('/challenges') : redirect('/challenge/edit')
+  if @challenge.save
+    send_mail(
+      :to => @challenge.to_player.email,
+      :from => @challenge.from_player.email,
+      :subject => "Updated Challenge on Latter",
+      :template => "challenge_updated",
+      :object => @challenge
+    )
+    redirect '/challenges'
+  else
+    redirect "/challenge/#{@challenge.id}/edit"
+  end
 end
 
 
@@ -188,18 +214,11 @@ def send_mail(options)
   @object = options[:object]
   Pony.mail(
     :to => options[:to],
-    :from => MAIL_FROM,
+    :from => options[:from],
     :subject => options[:subject],
-    :body => haml(:"mail/#{options[:template]}"),
+    :html_body => haml(:"mail/#{options[:template]}", :layout => false),
     :via => :smtp,
-    :via_options => {
-      :address => 'smtp.sendgrid.net',
-      :port => '25',
-      :authentication => :plain,
-      :user_name => ENV['SENDGRID_USERNAME'],
-      :password => ENV['SENDGRID_PASSWORD'],
-      :domain => ENV['SENDGRID_DOMAIN']
-    }
+    :via_options => PONY_SMTP_OPTIONS
   )
 end
 
@@ -213,5 +232,19 @@ end
 
 def authenticate!
   redirect '/' unless @current_player
+end
+
+def authenticate?(request)
+  path = request.path
+  case path
+  when /\A\/\Z/
+    return false
+  when /\A\/login\Z/
+    return false
+  when /\A\/setup\A/
+    return false
+  end
+
+  return true
 end
 
