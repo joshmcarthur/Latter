0da4932b598208fdd182ec4b67ba23c8a891e1bd
Josh McArthur <joshua.mcarthur@gmail.com>
2012-03-06 16:31:38 +1300

    Use FactoryGirl instead of Factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,13 +1,13 @@
-Factory.sequence(:player_name) do |n|
+FactoryGirl.sequence(:player_name) do |n|
   "player#{n}"
 end
 
-Factory.define(:player) do |player|
+FactoryGirl.define(:player) do |player|
   player.name { Factory.next(:player_name) }
   player.email { |u| "#{u.name}@3months.com" }
 end
 
-Factory.define(:challenge) do |chal|
+FactoryGirl.define(:challenge) do |chal|
   chal.association :from_player, :factory => :player
   chal.association :to_player, :factory => :player
   chal.completed false
