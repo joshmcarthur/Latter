a3340f0c38fad38f826a122019340b826ff21628
Josh McArthur <joshua.mcarthur@gmail.com>
2012-03-16 08:40:49 +1300

    Get rid of unused JS

--- public/js/application.js
+++ public/js/application.js
@@ -2,6 +2,7 @@ jQuery(function()
 {
   challengeButton()
   enterScoreButton()
+  //playerRankingHack()
 });
 
 var challengeButton = function() {
@@ -31,24 +32,14 @@ var numbers_only = function(elem) {
     elem.val(elem.val().replace(/[^0-9]/gi, ""));
 }
 
-var set_result = function(from_player_name, to_player_name)
-{
-    var from_score = $('section#scores input#from_player_score');
-    var to_score = $('section#scores input#to_player_score');
-    var result = $('section#scores span.result');
-    numbers_only(from_score);
-    numbers_only(to_score);
+var playerRankingHack = function() {
+  console.log('Fired')
+  var resize_ranking = function() {
+    $('.player .ranking-container').each(function(index, element) {
+      $(element).height($(element).parent().find('.thumbnail img').height());
+    });
+  }
 
-    if(parseInt(from_score.val()) > parseInt(to_score.val()))
-    {
-        result.text("= " + from_player_name + " won!");
-    }
-    else if(parseInt(from_score.val()) < parseInt(to_score.val()))
-    {
-        result.text("= " + to_player_name + " won!");
-    }
-    else if(parseInt(from_score.val()) == parseInt(to_score.val()))
-    {
-        result.text("= It's a draw!");
-    }
+  resize_ranking()
+  $(window).resize(resize_ranking)
 }
