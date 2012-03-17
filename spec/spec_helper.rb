35e65743d9c010e0e16656097e079c7fc3671ca4
Josh McArthur <joshua.mcarthur@gmail.com>
2012-03-17 20:37:44 +1300

    Tidy up spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,19 +1,27 @@
 require 'bundler/setup'
 Bundler.require
+
+# Require RSpec helpers
 require 'capybara/rspec'
 require 'rack/test'
 require 'factory_girl'
+
+# Load FactoryGirl definitions
 FactoryGirl.find_definitions
 
+# Require application
 require File.join(File.dirname(__FILE__), '..', 'latter')
-Dir[File.join(File.dirname(__FILE__), 'support', '*.rb')].each { |file| require file }
 
-set :environment, :test
+# Require supporting files
+Dir[File.join(File.dirname(__FILE__), 'support', '*.rb')].each { |file| require file }
 
 RSpec.configure do |config|
   config.before(:all)  do
     Capybara.app = Latter
     DataMapper.auto_migrate!
+
+    # Make sure we are testing in a sandbox by deleting
+    # existing Players and Challenges
     Player.destroy
     Challenge.destroy
   end
