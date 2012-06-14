5e71a2dde7b008d1b588f3fca4e7f1d9e272f3dd
Josh McArthur <joshua.mcarthur+git@gmail.com>
2012-06-15 11:04:24 +1200

    Run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120604204421) do
+ActiveRecord::Schema.define(:version => 20120614221427) do
 
   create_table "activities", :force => true do |t|
     t.text     "message",    :null => false
@@ -52,8 +52,13 @@ ActiveRecord::Schema.define(:version => 20120604204421) do
     t.boolean  "starter",                :default => true,  :null => false
     t.datetime "created_at",                                :null => false
     t.datetime "updated_at",                                :null => false
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.boolean  "changed_password",       :default => false, :null => false
   end
 
+  add_index "players", ["confirmation_token"], :name => "index_players_on_confirmation_token", :unique => true
   add_index "players", ["email"], :name => "index_players_on_email", :unique => true
   add_index "players", ["reset_password_token"], :name => "index_players_on_reset_password_token", :unique => true
 
