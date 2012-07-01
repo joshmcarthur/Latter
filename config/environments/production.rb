8e05803c77df0af478c838a2f2a20963c1fe6020
Josh McArthur <joshua.mcarthur+git@gmail.com>
2012-07-01 16:54:45 +1200

    Update hosts to link to for mailing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,9 @@ Latter::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
+  config.action_mailer.default_url_options = { :host => 'latter.herokuapp.com' }
+  config.action_mailer.asset_host = "http://latter.herokuapp.com"
+
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
