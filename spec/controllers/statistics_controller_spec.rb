e334e71ee06e96d1922a4f7327784ade5d40fe7b
Josh McArthur <joshua.mcarthur@gmail.com>
2012-07-17 22:00:35 +1200

    Write tests for StatisticsController#index

--- spec/controllers/statistics_controller_spec.rb
+++ spec/controllers/statistics_controller_spec.rb
@@ -2,4 +2,18 @@ require 'spec_helper'
 
 describe StatisticsController do
 
+  login_player
+
+  describe "GET index" do
+    it "should assign statistics to @data" do
+      get :index
+      assigns(:data).symbolize_keys.should eq Game.statistics
+    end
+
+    it "should render the index template" do
+      get :index
+      response.should render_template 'index'
+    end
+  end
+
 end
