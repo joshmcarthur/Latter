ca7a36e052c0992e895105299a44f92e2e336ed8
Josh McArthur <joshua.mcarthur@gmail.com>
2012-07-17 21:58:46 +1200

    Add specs for PlayerHelper#trend(player)

--- spec/helpers/players_helper_spec.rb
+++ spec/helpers/players_helper_spec.rb
@@ -11,5 +11,24 @@ require 'spec_helper'
 #   end
 # end
 describe PlayersHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @player = FactoryGirl.build(:player)
+  end
+
+  describe "#trend" do
+    it "should render a title if the player is improving" do
+      @player.should_receive(:trend).and_return(:up)
+      helper.trend(@player).should include "Improving"
+    end
+
+    it "should render a title if the player is worsening" do
+      @player.should_receive(:trend).and_return(:down)
+      helper.trend(@player).should include "Worsening"
+    end
+
+    it "should not render anything if the player trend has not changed" do
+      @player.should_receive(:trend).and_return(:same)
+      helper.trend(@player).should be_blank
+    end
+  end
 end
