6c20d0c813015d7534e1aab10cc08df75e4ad183
Daniel Sherson <daniel@3months.com>
2012-07-21 01:00:02 +1200

    Initial Design Pass 1. added type kit 2. added basic blackboard styling 3. added floaty taped avatars 4. messed with a couple of helpers 5. I've probably broken all of josh's pretty tests.

--- app/assets/javascripts/bootstrap.js.coffee
+++ app/assets/javascripts/bootstrap.js.coffee
@@ -2,3 +2,10 @@ jQuery ->
   $("a[rel=popover]").popover()
   $(".tooltip").tooltip()
   $("a[rel=tooltip]").tooltip()
+
+  $('body').noisy({
+    'intensity' : 1,
+    'size' : '200',
+    'opacity' : 0.053,
+    'monochrome' : false
+  })
--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -16,7 +16,8 @@ module PlayersHelper
         :title => 'Improving in the last 48 hours',
         :class => 'icon-chevron-up trend'
       )
-    when :down
+    #when :down
+    else
       content_tag(
         :a,
         '',
@@ -24,8 +25,8 @@ module PlayersHelper
         :title => 'Worsening in the last 48 hours',
         :class => 'icon-chevron-down trend'
       )
-    else
-      ""
+    # else
+    #   ""
     end
   end
 
@@ -42,35 +43,77 @@ module PlayersHelper
     end
   end
 
-  def primary_action_button_for(player)
-    if current_player == player
-      link_to edit_player_path(player), :class => 'btn btn-large' do
-        content_tag(:i, '', :class => 'icon-user') +\
-          I18n.t('player.edit')
-      end
-    elsif current_player and !current_player.in_progress_games(player).empty?
-      enter_score_options = {
-        :remote => true,
-        :'data-loading-text' => I18n.t('game.complete.link_loading'),
-        :class => 'btn btn-large btn-with-loading'
-      }
+  def player_class player
+    return '' unless current_player
+    return 'current' if current_player == player
+    return 'challenged' if current_player.in_progress_games(player).present?
+    return 'opponent'
+  end
 
-      link_to new_game_score_path(current_player.in_progress_games(player).first), enter_score_options do
-        content_tag(:i, '', :class => 'icon-plus-sign') +\
-          I18n.t('game.complete.link')
-      end
-    elsif current_player
-      challenge_link_options = {
-        :method => :post,
-        :remote => true,
-        :'data-loading-text' => I18n.t('game.new.link_loading'),
-        :class => 'btn btn-large btn-with-loading challenge'
-      }
+  def link_to_primary_action_for(player, content_or_options={}, options={}, &block)
+    if block_given?
+      options = content_or_options
+      content_or_options = capture(&block)
+    end
 
-      link_to games_path(:game => {:challenged_id => player.id}), challenge_link_options do
-        content_tag(:i, '', :class => 'icon-screenshot') +\
-          I18n.t('game.new.link')
+    if current_player
+      if current_player == player
+        #edit player
+        icon_class = 'icon-edit edit'
+        icon_label = t 'player.edit'
+        path = edit_player_path(player)
+      elsif current_player.in_progress_games(player).empty?
+        #initiate Challenge
+        icon_class = 'icon-screenshot'
+        icon_label = t 'game.new.link'
+        path = games_path(:game => {:challenged_id => player.id})
+        options.merge!(:remote => true, :method => :post)
+      else
+        #conclude challenge (enter score)
+        icon_class = 'icon-screenshot challenged'
+        icon_label = t 'player.challenged'
+        path = new_game_score_path(current_player.in_progress_games(player).first)
+        options.merge!(:remote => true)
       end
+      link_to path, options do
+        [content_or_options,
+         content_tag(:i, '', :class => icon_class),
+         content_tag(:span, icon_label)].join.html_safe
+      end
+    else
+      content_tag :span, content_or_options, options.reverse_merge(:class => 'a')
     end
   end
 end
+
+#     if current_player == player
+#       link_to edit_player_path(player), :class => 'btn btn-large' do
+#         content_tag(:i, '', :class => 'icon-user') +\
+#           I18n.t('player.edit')
+#       end
+#     elsif current_player and !current_player.in_progress_games(player).empty?
+#       enter_score_options = {
+#         :remote => true,
+#         :'data-loading-text' => I18n.t('game.complete.link_loading'),
+#         :class => 'btn btn-large btn-with-loading'
+#       }
+
+#       link_to new_game_score_path(current_player.in_progress_games(player).first), enter_score_options do
+#         content_tag(:i, '', :class => 'icon-plus-sign') +\
+#           I18n.t('game.complete.link')
+#       end
+#     elsif current_player
+#       challenge_link_options = {
+#         :method => :post,
+#         :remote => true,
+#         :'data-loading-text' => I18n.t('game.new.link_loading'),
+#         :class => 'btn btn-large btn-with-loading challenge'
+#       }
+
+#       link_to games_path(:game => {:challenged_id => player.id}), challenge_link_options do
+#         content_tag(:i, '', :class => 'icon-screenshot') +\
+#           I18n.t('game.new.link')
+#       end
+#     end
+#   end
+# end
