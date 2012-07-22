83ad19a36de74ac816bba91c9ba3a26cd9ce4e68
Bret Richdale <bret@3months.com>
2012-07-23 07:37:51 +1200

    Created badgetype model

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120706103017) do
+ActiveRecord::Schema.define(:version => 20120722192921) do
 
   create_table "activities", :force => true do |t|
     t.text     "message",    :null => false
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "badgetypes", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "imageURL"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   create_table "games", :force => true do |t|
