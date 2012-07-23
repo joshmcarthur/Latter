d7c9d9c42dfecf3a2ea71f5e4fb5e48f4e9628d2
Josh McArthur <joshua.mcarthur@gmail.com>
2012-07-24 08:55:12 +1200

    Further I18n of controllers, models and views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,9 @@
 
 class PagesController < ApplicationController
 
-  rescue_from ActionView::MissingTemplate, :with => proc do |exp| 
+  rescue_from ActionView::MissingTemplate, :with => proc { |exp| 
   	render :text => I18n.t('page.not_found'), :status => :not_found 
-  end
+  }
 
   # GET /pages/1
   def show
