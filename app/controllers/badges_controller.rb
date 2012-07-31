9b66330dbfa1f12f89e8d7a2f92292300729d2a3
Josh McArthur <joshua.mcarthur@gmail.com>
2012-07-31 23:19:06 +1200

    Uncomment caches_action, set cache key based on last created date for Badge

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -1,6 +1,11 @@
 class BadgesController < ApplicationController
 before_filter :authenticate_player!
- # caches_action :index, :expires_in => 60.minutes
+
+caches_action :index, 
+	:expires_in => 60.minutes,
+	:cache_path => Proc.new { |controller|
+		controller.params.merge(:last_modified => Badge.last.try(:created_at))
+	}
 
  def index
    @badges = Badge.all
