50f109dc2e2418f7dc8b567325a702cf23ebd604
Josh McArthur <joshua.mcarthur@gmail.com>
2012-07-31 23:19:47 +1200

    We don't need to expire the cache on a time limit - using the latest created date as part of the cache key should do that for us

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -2,7 +2,6 @@ class BadgesController < ApplicationController
 before_filter :authenticate_player!
 
 caches_action :index, 
-	:expires_in => 60.minutes,
 	:cache_path => Proc.new { |controller|
 		controller.params.merge(:last_modified => Badge.last.try(:created_at))
 	}
