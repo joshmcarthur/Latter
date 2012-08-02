d051aefcf200fb128003f61e8506f8ccab46c2f3
Josh McArthur <joshua.mcarthur@gmail.com>
2012-08-03 08:34:23 +1200

    Touch player to refresh cache when player is awarded a badge

--- app/models/award.rb
+++ app/models/award.rb
@@ -5,8 +5,8 @@ class Award < ActiveRecord::Base
 
   validates_presence_of :badge_id, :player_id
   
-  belongs_to :badge  
-  belongs_to :player 
+  belongs_to :badge
+  belongs_to :player, :touch => true
   
   attr_accessible :badge_id, :player_id, :award_date, :badge
   
