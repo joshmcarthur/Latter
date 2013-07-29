ec0a6520a8d0243f5bf86f5aad1f475859f736de
Josh McArthur <joshua.mcarthur@gmail.com>
2013-07-30 00:04:37 +1200

    Fix secret token deprecation warning

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Latter::Application.config.secret_token = '7ac589d9048929cdbd057830334762bb23a71ab2d1a7f4b1daf8443f725ba4b911d38aeef1656e7e0755f87b82d16bee506744f386bb4c85e431146a416e2133'
+Latter::Application.config.secret_key_base = '2bb23a71ab2d1a7f4b1'
