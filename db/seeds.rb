b1e08219ba6c6b18117b536a420bc0e403e2dcec
Josh McArthur <joshua.mcarthur@gmail.com>
2014-01-23 10:49:48 +1300

    Simplify seeds file and ensure it works with rake db:reset

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,18 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+require 'factory_girl'
+FactoryGirl.find_definitions
+
 Badge.find_or_create_by_name('Silver Bull', image_url: "/images/badges/silverbull.png", description: "Completed over 140 chargeable hours in a month")
 Badge.find_or_create_by_name('Gold Bull', image_url: "/images/badges/goldbull.png", description: "Completed over 170 chargeable hours in a month")
 
-15.times do |n|
-	FactoryGirl.create(:player)
-end
 
-player = Player.first
-player.password = "password"
-player.password_confirmation = "password"
-player.confirmed_at = Time.now
-player.changed_password = true
-player.save!
+FactoryGirl.create_list(
+  :player,
+  15,
+  password: 'password',
+  password_confirmation: 'password',
+  confirmed_at: Time.now,
+  changed_password: true
+)
