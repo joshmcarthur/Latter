5dc8c6751c328dbf3d27e2864b79fb7677d4a29d
Josh McArthur <joshua.mcarthur@gmail.com>
2015-02-27 12:07:50 +1300

    Revert "upgrade devise a little and add secret key"
    This reverts commit bde162132f76f5ca121ab824750bdcad56b4ebdf.

--- app/models/player.rb
+++ app/models/player.rb
@@ -20,8 +20,8 @@ class Player < ActiveRecord::Base
          :confirmable,
          :recoverable,
          :trackable,
-         :validatable
-        
+         :validatable,
+         :token_authenticatable
 
   before_validation :set_default_password, :on => :create
   before_save :ensure_authentication_token
